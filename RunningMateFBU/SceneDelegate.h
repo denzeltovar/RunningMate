41113c0e893e9ee201c7cdeba66c027a23c29900
Denzel Tovar <denzeltov@fb.com>
2020-07-13 10:56:01 -0700

    Initial Commit

---
+++ RunningMateFBU/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  RunningMateFBU
+//
+//  Created by denzeltov on 7/13/20.
+//  Copyright © 2020 denzeltov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ RunningMateFBUTests/RunningMateFBUTests.m
@@ -0,0 +1,37 @@
+//
+//  RunningMateFBUTests.m
+//  RunningMateFBUTests
+//
+//  Created by denzeltov on 7/13/20.
+//  Copyright © 2020 denzeltov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RunningMateFBUTests : XCTestCase
+
+@end
+
+@implementation RunningMateFBUTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
