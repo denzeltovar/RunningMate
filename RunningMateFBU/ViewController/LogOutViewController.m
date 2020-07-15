29059c84dac0b4bd85a9fe76213e0c1fb2697c3b
Denzel Tovar <denzeltov@fb.com>
2020-07-15 14:36:46 -0700

    User can now log out

---
+++ RunningMateFBU/ViewController/LogOutViewController.m
@@ -0,0 +1,45 @@
+//
+//  LogOutViewController.m
+//  RunningMateFBU
+//
+//  Created by denzeltov on 7/15/20.
+//  Copyright © 2020 denzeltov. All rights reserved.
+//
+
+#import "LogOutViewController.h"
+#import "SceneDelegate.h"
+#import "LoginViewController.h"
+@import Parse;
+
+@interface LogOutViewController ()
+
+@end
+
+@implementation LogOutViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)didTapLogout:(id)sender {
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+        if (error == nil){
+            SceneDelegate *sceneDelegate = (SceneDelegate *)self.view.window.windowScene.delegate;
+            UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+            LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+            sceneDelegate.window.rootViewController = loginViewController;
+        }
+    }];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
